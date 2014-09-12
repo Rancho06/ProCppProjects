fd95de7a192248d7f01f88c8c1000e7b08caed77
Shu Zhou <shuzhou@usc.edu>
2014-09-12 03:56:10 -0700

    implemented RLE::Compress function for part 1

--- rle/rle-algo.hpp
+++ rle/rle-algo.hpp
@@ -43,11 +43,58 @@ struct RLE
 template <typename T>
 void RLE<T>::Compress(const T* input, int inSize)
 {
+	delete[] m_Data;
+	m_Size = 0;
+
+	int maxRunSize = MaxRunSize();
+	int typeSize = sizeof(T);
+	const T* temp = input;
+
+	m_Data = new T[inSize * 2];
+	int currentRunCount = 0;
+	
+	while (temp < (input + inSize)) {
+		currentRunCount = 0;
+		const T* start = temp;
+		if (*temp == *(temp + typeSize)) {
+			temp += typeSize;
+			currentRunCount = 2;
+			while (*temp == *(temp + typeSize) && currentRunCount < maxRunSize) {
+				temp += typeSize;
+				currentRunCount++;
+			}
+			temp++;
+			m_Data[m_Size] = currentRunCount;
+			m_Size++;
+			m_Data[m_Size] = *start;
+			m_Size++;
+		}
+		else {
+			currentRunCount = 1;
+			temp += typeSize;
+			while (*temp != *(temp + typeSize)) {
+				temp++;
+				currentRunCount++;
+			}
+			m_Data[m_Size] = 0 - currentRunCount;
+			m_Size++;
+			for (int i = 0; i < currentRunCount; i++) {
+				m_Data[m_Size] = *(start+i);
+				m_Size++;
+			}
+		}
+	}
+
 
 }
 
 template <typename T>
 void RLE<T>::Decompress(const T* input, int inSize, int outSize)
 {
+	//delete[] m_Data;
+	//m_Size = 0;
+
+	//m_Data = new T[outSize];
+
 
 }
--- rle/rle.cpp
+++ rle/rle.cpp
@@ -5,6 +5,7 @@
 #include "rle-algo.hpp"
 #include <iostream>
 #include "rle-files.h"
+//#include <sstream>
 
 void Part1Tests()
 {
@@ -27,22 +28,54 @@ void Part1Tests()
 	std::cout << r << std::endl << std::endl;
 }
 
+bool is_number(const std::string& s)
+{
+	std::string::const_iterator it = s.begin();
+	while (it != s.end() && isdigit(*it)) ++it;
+	return !s.empty() && it == s.end();
+}
+
 int main(int argc, char* argv[])
 {
 	// Main entry point!
-	std::string inputStream;
-	int choice = 0;
+	std::string inputString;
 
-	std::cout << "Please choose an opotion:" << std::endl;
-	std::cout << "1. Part 1 (Tests)" << std::endl;
-	std::cout << "2. Part 2 (Single File)" << std::endl;
-	std::cout << "3. Part 3 (Directories)" << std::endl;
-	std::cout << "4. Exit" << std::endl;
-	std::cout << "> ";
-	std::getline(std::cin,inputStream);
-	
+	int choice;
 
+	do {
+		inputString = "";
+		choice = 0;
 
+		std::cout << "Please choose an opotion:" << std::endl;
+		std::cout << "1. Part 1 (Tests)" << std::endl;
+		std::cout << "2. Part 2 (Single File)" << std::endl;
+		std::cout << "3. Part 3 (Directories)" << std::endl;
+		std::cout << "4. Exit" << std::endl;
+		std::cout << "> ";
+		std::getline(std::cin, inputString);
+
+		if (is_number(inputString)) {
+			choice = std::stoi(inputString);
+			switch (choice) {
+			case 1:
+				Part1Tests();
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				std::cout << "Exiting the program ..............." << std::endl;
+				return 0;
+			default:
+				std::cout << "Sorry, you should only enter 1, 2, 3 or 4 to continue." << std::endl;
+
+			}
+		}
+		else {
+			std::cout << "Sorry, please enter a positive integer number." << std::endl;
+		}
+	} while (true);
 
 	return 0;
 }
