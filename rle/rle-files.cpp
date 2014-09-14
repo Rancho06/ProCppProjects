95d51b5f7d78a824cb6f3e3f28a3a8c3074fe3c4
Shu Zhou <shuzhou@usc.edu>
2014-09-13 18:08:48 -0700

    implemented RLE_v1::CreateArchive() in part 2

--- rle/rle-files.cpp
+++ rle/rle-files.cpp
@@ -8,7 +8,48 @@
 
 void RLE_v1::CreateArchive(const std::string& source)
 {
+	// Requires <fstream>
+	std::ifstream::pos_type size;
+	char* memblock;
+	std::ifstream file(source, std::ios::in | std::ios::binary | std::ios::ate);
+	if (file.is_open())
+	{
+		size = file.tellg();
+		memblock = new char[static_cast<unsigned int>(size)];
+		file.seekg(0, std::ios::beg);
+		file.read(memblock, size);
+		file.close();
+		// File data has now been loaded into memblock array
 
+		m_Data.Compress(memblock, (int)size);
+
+		std::cout << "Compression: " << (double)((int)size - m_Data.m_Size) * 100 / ((int)size) << "%." << std::endl << std::endl;
+
+		m_Header.sig[0] = 'R';
+		m_Header.sig[1] = 'L';
+		m_Header.sig[2] = 'E';
+		m_Header.sig[3] = 0x01;
+		m_Header.fileSize = static_cast<int>(size);
+		m_Header.fileNameLength = source.length();
+		m_Header.fileName = source;
+		
+		std::ofstream arc(source+".rl1", std::ios::out | std::ios::binary | std::ios::trunc);
+		if (arc.is_open())
+		{
+			// Use arc.write function to write data here
+			arc.write(m_Header.sig,4);
+			arc.write(reinterpret_cast<char*>(&(m_Header.fileSize)), 4);
+			arc.write(reinterpret_cast<char*>(&(m_Header.fileNameLength)), 1);
+			arc.write(m_Header.fileName.c_str(),m_Header.fileNameLength);
+			arc.write(m_Data.m_Data, m_Data.m_Size);
+			arc.close();
+		}
+		// Make sure to clean up!
+		delete[] memblock;
+	}
+	else {
+		std::cout << "Sorry, file name not found!" << std::endl << std::endl;
+	}
 }
 
 void RLE_v1::ExtractArchive(const std::string& source)
--- rle/rle.cpp
+++ rle/rle.cpp
@@ -35,12 +35,25 @@ bool is_number(const std::string& s)
 	return !s.empty() && it == s.end();
 }
 
+void singleFileCreate(const std::string& fileName) {
+	RLE_v1* singleFileController = new RLE_v1();
+	singleFileController->CreateArchive(fileName);
+	delete singleFileController;
+}
+
+void singleFileExtract(const std::string& fileName) {
+	RLE_v1* singleFileController = new RLE_v1();
+	singleFileController->ExtractArchive(fileName);
+	delete singleFileController;
+}
+
 int main(int argc, char* argv[])
 {
 	// Main entry point!
 	std::string inputString;
 
 	int choice;
+	bool isPartTwo = false, isPartThree = false;
 
 	do {
 		inputString = "";
@@ -53,6 +66,7 @@ int main(int argc, char* argv[])
 		std::cout << "4. Exit" << std::endl;
 		std::cout << "> ";
 		std::getline(std::cin, inputString);
+		std::cout << std::endl;
 
 		if (is_number(inputString)) {
 			choice = std::stoi(inputString);
@@ -61,19 +75,91 @@ int main(int argc, char* argv[])
 				Part1Tests();
 				break;
 			case 2:
+				isPartTwo = true;
+				do {
+					std::cout << "You are in Part 2 (Single File)." << std::endl;
+					std::cout << "Please choose from the following options:" << std::endl;
+					std::cout << "1. Create an archive" << std::endl;
+					std::cout << "2. Extract an archive" << std::endl;
+					std::cout << "3. Return back to the main menu" << std::endl;
+					std::cout << "> ";
+					std::getline(std::cin, inputString);
+					std::cout << std::endl;
+
+					if (is_number(inputString)) {
+						choice = std::stoi(inputString);
+						switch (choice) {
+							case 1:
+								std::cout << "Please enter the name of the file you wish to compress: " << std::endl;
+								std::cout << "> ";
+								std::getline(std::cin, inputString);
+								std::cout << std::endl;
+								singleFileCreate(inputString);
+								break;
+							case 2:
+								std::cout << "Please enter the name of the file you wish to extract: " << std::endl;
+								std::cout << "> ";
+								std::getline(std::cin, inputString);
+								std::cout << std::endl;
+								singleFileExtract(inputString);
+								break;
+							case 3:
+								isPartTwo = false;
+								std::cout << "Returning to the Main Menu........." << std::endl << std::endl;
+								break;
+							default:
+								std::cout << "Sorry, you should only enter 1, 2 or 3 to continue." << std::endl << std::endl;
+								break;
+						}
+					}
+					else {
+						std::cout << "Sorry, please enter a positive integer number." << std::endl << std::endl;
+					}
+				} while (isPartTwo);
 				break;
 			case 3:
+				isPartThree = true;
+				do {
+					std::cout << "You are in Part 3 (Directories)." << std::endl;
+					std::cout << "Please choose from the following options:" << std::endl;
+					std::cout << "1. Create an archive" << std::endl;
+					std::cout << "2. Extract an archive" << std::endl;
+					std::cout << "3. Return back to the main menu" << std::endl;
+					std::cout << "> ";
+					std::getline(std::cin, inputString);
+					std::cout << std::endl;
+
+					if (is_number(inputString)) {
+						choice = std::stoi(inputString);
+						switch (choice) {
+						case 1:
+							break;
+						case 2:
+							break;
+						case 3:
+							isPartThree = false;
+							std::cout << "Returning to the Main Menu........." << std::endl << std::endl;
+							break;
+						default:
+							std::cout << "Sorry, you should only enter 1, 2 or 3 to continue." << std::endl << std::endl;
+							break;
+						}
+					}
+					else {
+						std::cout << "Sorry, please enter a positive integer number." << std::endl << std::endl;
+					}
+				} while (isPartThree);
 				break;
 			case 4:
-				std::cout << "Exiting the program ..............." << std::endl;
+				std::cout << "Exiting the program ..............." << std::endl <<std::endl;
 				return 0;
 			default:
-				std::cout << "Sorry, you should only enter 1, 2, 3 or 4 to continue." << std::endl;
+				std::cout << "Sorry, you should only enter 1, 2, 3 or 4 to continue." << std::endl << std::endl;
 
 			}
 		}
 		else {
-			std::cout << "Sorry, please enter a positive integer number." << std::endl;
+			std::cout << "Sorry, please enter a positive integer number." << std::endl <<std::endl;
 		}
 	} while (true);
 
